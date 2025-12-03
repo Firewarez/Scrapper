8af081e5751526f6ede86dd1d791165d1aa8fc71
Arthur <162390657+Firewarez@users.noreply.github.com>
2025-12-03 18:01:31 -0300

    style: Just formating the code and adding flux control to elements

--- main.py
+++ main.py
@@ -24,17 +24,23 @@ soup = BeautifulSoup(response.text, 'html.parser')
 cards = soup.find_all('a', class_='sympla-card')
 
 # Find Title of the event
-title = soup.find_all('h3', class_='pn67h1c')
+#title = soup.find_all('h3', class_='pn67h1c')
 
 # Find P element for event location
-location = soup.find_all('p', class_='pn67h1e')
+#location = soup.find_all('p', class_='pn67h1e')
 
 # Find Div element for event date and time
-dates = soup.find_all('div', class_='qtfy415 qtfy413 qtfy416')
+#dates = soup.find_all('div', class_='qtfy415 qtfy413 qtfy416')
 
 for card in cards:
-    card_title = card.find('h3', class_='pn67h1c').get_text()
-    card_location = card.find('p', class_='pn67h1e').get_text()
-    card_date = card.find('div', class_='qtfy415 qtfy413 qtfy416').get_text()
-    print(f'Title: {card_title}\nLocation: {card_location}\nDate: {card_date}\n')
+    card_title_element = card.find('h3', class_='pn67h1c')
+    title = card_title_element.get_text() if card_title_element else "Title not found"
+    
+    card_location_element = card.find('p', class_='pn67h1e')
+    location = card_location_element.get_text() if card_location_element else "Location not found"
+    
+    card_date_element = card.find('div', class_='qtfy415 qtfy413 qtfy416')
+    date = card_date_element.get_text() if card_date_element else "Date not found"
+    
+    print(f'Title: {title}\nLocation: {location}\nDate: {date}\n')
 
